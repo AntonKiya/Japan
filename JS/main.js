efccbc2bdd9f07235138794930f1bb489f2d13b2
AntonKiya <49375431+AntonKiya@users.noreply.github.com>
2020-10-20 21:38:55 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -8,5 +8,17 @@
             header.classList.remove( 'header_active')
         }
     }
-}())
+}());
 
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const close = document.querySelector('.header__nav-close')
+
+burger.addEventListener('click',evt => {
+    menu.classList.add('header__nav__active');
+});
+
+close.addEventListener('click', evt => {
+    menu.classList.remove('header__nav__active');
+})
